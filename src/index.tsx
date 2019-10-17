e2e4833a9d011d012bdaf903b1086d94a58524d9
TakutoOkuno <takuto.okuno.ku0823@gmail.com>
2019-10-17 22:19:50 +0900

    complete ready for tutorial

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,53 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+const Square: React.FunctionComponent = () =>
+    <button className="square">
+        {/* TODO */}
+    </button>;
+
+const Board: React.FunctionComponent = () => {
+    const renderSquare = (i: number) => {
+        return <Square />;
+    };
+
+    const status = 'Next player: X';
+
+    return <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+        </div>
+        <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+    </div>;
+};
+
+const Game: React.FunctionComponent = () =>
+    <div className="game">
+        <div className="game-board">
+            <Board />
+        </div>
+        <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+        </div>
+    </div>;
+
+// ========================================
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Game />,
+    document.getElementById('root')
 );
-registerServiceWorker();
