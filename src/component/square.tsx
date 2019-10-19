d593ce3a7c88919012963e5e89e578b9917ce1a1
TakutoOkuno <takuto.okuno.ku0823@gmail.com>
2019-10-20 02:58:05 +0900

    feat: make square component interactive

--- src/component/square.tsx
+++ src/component/square.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
+import {useState} from "react";
 import '../index.css';
 
 interface Props {
-    value: number;
+    value: number | 'X';
 }
 
 const Square: React.FunctionComponent<Props> = (
     {
-        value,
+        value: initialValue
     }: Props
-) =>
-    <button className="square">
+) => {
+    const [value, setValue] = useState(initialValue);
+    const handleClick = () => setValue('X');
+    return <button className="square" onClick={handleClick}>
         {value}
-    </button>;
+    </button>
+};
 
 export default Square;
