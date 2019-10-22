15def82f165e3cfb1bc886ce854ef0a4535e1274
TakutoOkuno <takuto.okuno.ku0823@gmail.com>
2019-10-22 15:30:14 +0900

    feat: implement calculating winner feature

--- src/component/board.tsx
+++ src/component/board.tsx
@@ -8,6 +8,9 @@ const Board: React.FunctionComponent = () => {
     const [xIsNext, setXIsNext] = useState(true);
     const handleClick = (i: number) => {
         const newSquares = squares.slice();
+        if (winner || squares[i]) {
+            return;
+        }
         newSquares[i] = xIsNext ? 'X' : 'O';
         setSquares(newSquares);
         setXIsNext(!xIsNext);
@@ -19,11 +22,33 @@ const Board: React.FunctionComponent = () => {
             onClick={() => handleClick(i)}/>;
     };
 
-    const status = 'Next player: X';
     const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    const calculateWinner = (calcSquares: ReadonlyArray<string>) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (const l of lines) {
+            const [a, b, c] = l;
+            if (calcSquares[a] && calcSquares[a] === calcSquares[b] && calcSquares[a] === calcSquares[c]) {
+                return calcSquares[a];
+            }
+        }
+        return null;
+    };
+
+    const winner = calculateWinner(squares);
+    const winnerStatus = `Winner: ${winner}`;
 
     return <div>
-        <div className="status">{status}</div>
+        <div className="status">{winner ? winnerStatus : status}</div>
         <div className="board-row">
             {renderSquare(0)}
             {renderSquare(1)}
